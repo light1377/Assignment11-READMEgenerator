7eeab8c3c9e4d370aef9ae1dd30247274c2cb8fd
Moral <moral@meshkot.com>
2023-02-05 00:21:38 +0000

    Added walkthrough demonstration

--- READMESample.md
+++ READMESample.md
@@ -1,9 +1,9 @@
 
-# sbry
-[![license: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+# README generator
+[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)
 
 ## Description:
-    h
+In this project a README file will be generated automatically for the user. It can be used for any project in GitHub.
 ## Contents:
 
 - [Installation](#installation-instructions)
@@ -13,26 +13,23 @@
 - [Tests](#tests)
 - [Questions](#questions)
 
-
-
-
 ## Installation Instructions
-fh
+npm init -y
 
 ## Usage
-ftghv
+'It can be used to generate a README file for any future project which would save a lot of time.'
 
 ## License
-MIT
+GPL
 
 ## How to contribute
-rfh
+This project can be improved by asking more general questions so it won't just be limited to GitHub users.
 
 ## Tests
-frtg
+npm test
 
 ## Questions
 
-This project was made by senrbyd, please see their GitHub profile [here](https://github.com/senrbyd)
+This project was made by GitHub user light1377, please see their GitHub profile [here](https://github.com/light1377)
 
-Additionally, you can contact them via fsrnydb for any questions or comments. 
+Additionally, you can contact them via Moral@Meshkot.com for any questions or comments. 
--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const path = require('path');
 const util = require('util');
 const writeFileAsync = util.promisify(fs.writeFile);
 
-//not sure what this line does but it seems the generator can work without it:
-//const generateMarkdown = require("./utils/generateMarkdown");
-
 inquirer.prompt([
     {
         type: 'input',
@@ -55,12 +52,6 @@ inquirer.prompt([
         message: 'How can this project be used?',
     },
 ])
-    // .then(function (responses) {
-    //     for(let i = 0; i < responses.channels; i++) {
-    //         if (responses.license === 0) {
-    //             console.log("You are probably smart");
-    //         }}
-    //     })
     .then((data) => {
         console.log(data);
 
@@ -78,47 +69,12 @@ inquirer.prompt([
 
         }
 
-
-        // const li = data.license;
-        // let x = li
-        // console.log(li);
-        // if (li === "MIT") {
-        //     console.log("MITTTTTTTT")
-        //     let x = "[![license: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
-        // }
-        // else if (li === "APACHE") {
-        //     console.log("APACHEEE");
-        //     x = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
-        // }
-        // else if (li === "GPL") {
-        //     console.log("GPLLLLL");
-        //     x = "[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)"
-        // }
-        // else if (li === "BSD") {
-        //     console.log("BSDDDDDD");
-        //     x = "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
-        // }
-        // else if (li === "none") {
-        //     console.log("no license");
-        // }
-        // console.log(x);
-
-
-
-        // const licenses:
-        // this.MIT = [![license: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT),
-        // this.APACHE =
-        // this.GPL = 
-        // this.BSD = 
-
-
-
         const mdInput = `
 # ${title}
 ${licenseBadge(data.license)}
 
 ## Description:
-    ${ description }
+${ description }
 ## Contents:
 
 - [Installation](#installation-instructions)
@@ -127,9 +83,6 @@ ${licenseBadge(data.license)}
 - [Contributing](#how-to-contribute)
 - [Tests](#tests)
 - [Questions](#questions)
-
-
-
 
 ## Installation Instructions
 ${installation}
@@ -148,70 +101,11 @@ ${tests}
 
 ## Questions
 
-This project was made by ${username}, please see their GitHub profile [here](https://github.com/${username})
+This project was made by GitHub user ${username}, please see their GitHub profile [here](https://github.com/${username})
 
 Additionally, you can contact them via ${email} for any questions or comments. 
 `;
         fs.writeFile("READMESample.md", mdInput, (err) =>
             err ? console.log(err) : console.log('Success!')
         );
-
-
-    })
-
-
-
-
-// .then((data) => {
-
-// const {name, description} = data;
-// const mdContent = `
-// <title>${data.title}</title>
-// <h1>${data.title}</h1>
-// Project Details:
-// <br>
-// <p> Description: ${data.description}</p>
-
-// `;
-
-// questions()
-// .then((answers) = writeFileAsync('ReadmeGen.md', generateReadme(answers)))
-// .then(() => console.log('README generated!'))
-// .catch((err) => console.error(err))
-
-
-
-// // function to write README file
-// function writeToFile(fileName, data) {
-//     const { title, username, email, description, installation, contribute, tests, license, contents } = data;
-//     fs.writeFile("index.html", markup) = {
-//         //err? console.log(err) ; 
-//         console.log("Success");
-
-//     };
-// }
-
-// // function to initialize program
-// function init() {
-//     inquirer
-//         .prompt(questions)
-//         .then((data) => {
-//             const { title, usernme, email, description, installation, contribute, tests, license, contents } = data;
-//             console.log(data);
-//             writeToFile(title, data);
-//         });
-// }
-
-// from activity 9 of class 2:
-//.then((response) =>
-// response.confirm === response.password
-//   ? console.log('Success!')
-//   : console.log('You forgot your password already?!')
-// );
-// from activity 10 of class 2:
-// .then((data) => {
-//     const filename = `${data}.json`;
-//     fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-//     err ? console.log(err) : console.log('Success!')
-// );
-// });
+    });
